ef45d00345118298daf81e8e98030ff170514036
Janna Pakarinen <janne.pakarinen@helsinki.fi>
2013-12-10 01:58:19 +0200

    Lisätty Ruutu-luokalle tuki erilaisille pistearvoille

--- Javeled/src/sovelluslogiikka/Ruutu.java
+++ Javeled/src/sovelluslogiikka/Ruutu.java
@@ -18,13 +18,24 @@ package sovelluslogiikka;
 public class Ruutu {
     
     private int tyyppi;
+    private int pistearvo;
     
     /**
      * Konstruktori, joka luo halutun tyyppisen ruudun
      * @param tyyppi haluttu tyyppi
      */
     public Ruutu(int tyyppi) {
+        this(tyyppi, 1);
+    }
+    
+    /**
+     * Konstruktori, joka luo halutun tyyppisen ruudun jollain pistearvolla.
+     * @param tyyppi haluttu tyyppi
+     * @param pistearvo ruudun antama pistemäärä
+     */
+    public Ruutu(int tyyppi, int pistearvo) {
         this.tyyppi = tyyppi;
+        this.pistearvo = pistearvo;
     }
     
     /**
@@ -45,6 +56,24 @@ public class Ruutu {
         this.tyyppi = uusiTyyppi;
     }
     
+    /**
+     * 
+     * @return ruudun antama pistemäärä
+     */
+    public int pistearvo() {
+        return this.pistearvo;
+    }
     
+    /**
+     * Setteri ruudun pistearvolle
+     * @param arvo haluttu pistearvo
+     */
+    public void asetaPistearvo(int arvo) {
+        if (arvo > 0) {
+            this.pistearvo = arvo;
+        } else {
+            this.pistearvo = 1;
+        }
+    }
     
 }
